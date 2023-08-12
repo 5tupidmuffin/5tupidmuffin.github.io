2991c976aae38d342daf2b7571067b88c45ad842
5tupidmuffin <rameshwark9500@gmail.com>
2023-08-12 20:45:15 +0530

    contact page done

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+import React from "react";
+import ContactPageComponent from "@/components/contact";
+
+const ContactPage: NextPage = () => {
+  return <ContactPageComponent />;
+};
+
+export default ContactPage;
