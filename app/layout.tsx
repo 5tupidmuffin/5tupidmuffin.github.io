29c271d69ba65fbb50a5503ff27a6a9ecab8be6c
5tupidmuffin <rameshwark9500@gmail.com>
2023-09-13 21:05:15 +0530

    added meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,22 @@ export default function RootLayout({
           type="image/x-icon"
           href="https://avatars.githubusercontent.com/u/43025025?v=4" // github pfp
         ></link>
+        <link rel="canonical" href="https://5tupidmuffin.github.io/" />
+        <meta property="og:locale" content="en_IN" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:site_name"
+          content="Rameshwar Kamble | 5tupidmuffin"
+        />
+        <meta name="description" content="Portfolio of 5tupidmuffin" />
+        <meta property="og:title" content="Rameshwar Kamble | 5tupidmuffin" />
+        <meta
+          property="og:description"
+          content="Portfolio of 5tupidmuffin"
+        />
+        <meta property="og:url" content="https://5tupidmuffin.github.io/" />
+        <meta name="google" content="nositelinkssearchbox" />
+        <meta name="google" content="notranslate" />
       </head>
       <body>{children}</body>
     </html>
